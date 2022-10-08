d6e11d0b2dc129c8b38b596b548efe2450363630
Darshan Lokhande <lokhande.darshan25@gmail.com>
2022-10-08 13:46:36 +0530

    Add recommendation API

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,8 @@
-from flask import Flask
+# Import libraries
+import math
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -13,3 +17,23 @@ def getfile():
     else:
         result = request.args.get['myfile']
     return result
+
+@app.route('/getRecommendation')
+def getRecommendation():
+    args = request.args
+    param1 = str(args.get('param1'))
+    # recommendations for the account
+    recommendations = []
+    find_recommendations(param1, recommendations)
+    recommendations = get_max_two_recommendations(recommendations)
+    responseObjList = []
+    for data in recommendations : 
+        responseObj = {}
+        responseObj['text'] = data['text']
+        responseObj['rank'] = data['rank']
+        responseObjList.append(responseObj)
+    return jsonify(responseObjList)
+
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
